a24c49f963d899fc3cde675d121c61b33c5c4ff2
JARVVVIS <ruchitr.ec.17@nsit.net.in>
2025-08-12 10:03:38 -0400

    scripts for vision-reward parr.

--- scripts/openvid_run.sh
+++ scripts/openvid_run.sh
@@ -1,3 +1,4 @@
+## Base Run
 python run_metrics_openvid1m.py \
     --filters laion_aesthetics \
     --num_gpus 2 \
@@ -6,31 +7,32 @@ python run_metrics_openvid1m.py \
     --batch_size 25
 
 ## increase: batch_size, workers_per_gpu, num_gpus; as per your GPU memory
-## for more parallelization set start_idx and end_idx in run_metrics_openvid1m.py and launch parallel jobs
+## for more parallelization set start_idx and end_idx in run_metrics_openvid1m.py and launch these jobs parallelly
+python run_metrics_openvid1m.py \
+    --filters vision_reward \
+    --num_gpus 2 \
+    --workers_per_gpu 2 \
+    --checkpoint_freq 25 \
+    --batch_size 5 \
+    --start_idx 0 \
+    --end_idx 10000
 
-# python run_metrics_openvid1m.py \
-#     --filters laion_aesthetics \
-#     --num_gpus 2 \
-#     --workers_per_gpu 2 \
-#     --checkpoint_freq 25 \
-#     --batch_size 5 \
-#     --start_idx 0 \
-#     --end_idx 1000
+python run_metrics_openvid1m.py \
+    --filters vision_reward \
+    --num_gpus 2 \
+    --workers_per_gpu 2 \
+    --checkpoint_freq 25 \
+    --batch_size 5 \cla
+    --start_idx 10000 \
+    --end_idx 20000
 
-# python run_metrics_openvid1m.py \
-#     --filters laion_aesthetics \
-#     --num_gpus 2 \
-#     --workers_per_gpu 2 \
-#     --checkpoint_freq 25 \
-#     --batch_size 5 \
-#     --start_idx 1000 \
-#     --end_idx 2000
+python run_metrics_openvid1m.py \
+    --filters vision_reward \
+    --num_gpus 2 \
+    --workers_per_gpu 2 \
+    --checkpoint_freq 25 \
+    --batch_size 5 \
+    --start_idx 20000 \
+    --end_idx 30000
 
-# python run_metrics_openvid1m.py \
-#     --filters laion_aesthetics \
-#     --num_gpus 2 \
-#     --workers_per_gpu 2 \
-#     --checkpoint_freq 25 \
-#     --batch_size 5 \
-#     --start_idx 2000 \
-#     --end_idx 3000
+## ...
